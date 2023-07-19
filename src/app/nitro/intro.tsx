c38e643e34a80ff37df0443145930ae7eca288a2
Sahab <sahabferdous1@gmail.com>
2023-07-19 18:44:54 +0800

    Update nitro page

--- src/app/nitro/intro.tsx
+++ src/app/nitro/intro.tsx
@@ -4,8 +4,17 @@ import Navbar from "../components/Navbar";
 
 export default function Intro() {
   return (
-    <div className="bg-notQuiteBlack overflow-hidden w-full flex flex-col items-center">
+    <div>
       <Navbar />
+      <div className="nitro-bg pb-[1049px] overflow-hidden h-auto pt-[200px] justify-center relative flex flex-col items-center -mt-[82px]">
+        <div className="max-w-[1120px] static overflow-visible justify-start items-center flex flex-col w-[90%] h-auto self-start mx-auto">
+          <div className="z-50 text-center justify-start self-center items-center max-w-[420px] relative flex flex-col">
+            <h1 className="text-4xl text-center text-white font-headline mb-6">
+              Unleash more fun with Nitro
+            </h1>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
